423baa341fd269a2497892a36efbd973b8e6c98e
DroneBreaker <brownierz01@gmail.com>
2022-11-27 01:37:24 +0000

    Initializing the rest of the backend

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -18,7 +17,7 @@ export default function Navbar() {
   return (
     <div className='bg-white border-b-2 shadow-2xl shadow-neutral-400'>
         <nav className='md:mx-24 mx-10 md:flex'>
-            <img alt='VM' className='text-left m-2 h-8 w-8 rounded-full' src='/voguesmaven.jpg'/>
+            <img alt='VM' className='text-left m-2 h-9 w-9 rounded-full' src='/voguesmaven.jpg'/>
             
             <ul className='m-2' key={'item'}>
                 {navItems.map((item, key) => (
