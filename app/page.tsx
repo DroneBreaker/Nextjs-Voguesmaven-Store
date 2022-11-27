016fcb8171e3e856bcb190e2210d9cc72ed80967
DroneBreaker <brownierz01@gmail.com>
2022-11-27 11:55:17 +0000

    adding content

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,15 @@ export default function Home() {
   return (
       <motion.div initial={{opacity: 0, scale: 0.2}} whileInView={{opacity: 1, scale: 1}} transition={{duration: 0.9}}>
         <div className="bg-neutral-300 h-[22rem] mb-4">
-          <div className="grid md:grid-cols-2 grid-cols-1">
+          <div className="grid grid-cols-2">
             <div>
-              <h2 className="md:mx-24 mx-10 md:text-5xl text-3xl font-medium mt-16 md:w-[24rem] w-[15rem] mb-4">Shop from us and get amazing deals</h2>
+              <h2 className="md:mx-24 mx-10 md:text-5xl text-3xl font-medium md:mt-16 mt-10 md:w-[24rem] w-[15rem] mb-4">Shop from us and get amazing deals</h2>
 
               <Button text="Shop Now" route="/shop"/>
             </div>
 
-            <div className="relative">
-              <img style={{top: '15%', left: '20%'}} className="absolute" src="#"/>
+            <div className="relative bg-neutral-300">
+              <img style={{top: '3%', left: '5%'}} className="absolute h-[18rem] w-[17rem]" src="/home.png"/>
             </div>
           </div>
         </div>
